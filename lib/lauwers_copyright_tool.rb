ba5a17dcfb7249b724e097c53a04962f5dded9f3
Hans Lauwers <hans.lauwers@hotmail.com>
2018-06-29 14:58:03 -0700

    Initial commit copyright tool

---
+++ lib/lauwers_copyright_tool.rb
@@ -0,0 +1,6 @@
+require "lauwers_copyright_tool/version"
+require "lauwers_copyright_tool/renderer"
+
+module LauwersCopyrightTool
+  # Your code goes here...
+end
---
+++ lib/lauwers_copyright_tool/version.rb
@@ -0,0 +1,3 @@
+module LauwersCopyrightTool
+  VERSION = "0.1.0"
+end
